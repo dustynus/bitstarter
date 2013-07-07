0b4c9042b50f11510396d0c6934ed1d161b3f6c9
dustynus <stockermans@gmail.com>
2013-07-07 17:30:26 +0000

    update var fs

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var buffer = new Buffer('index');
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
 app.get('/', function(request, response) {
 buffer.write(fs.readFileSync(index.html), "utf-8");
 buffer.toString('utf-8');
